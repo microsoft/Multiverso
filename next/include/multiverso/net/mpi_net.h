f17bb7e36f2c6a070154eafdce73a9a9e53b17cc
feiga <gf0109@gmail.com>
2016-02-26 12:46:58 +0800

    shorten function name, change the way to using mpi msg handle

--- next/include/multiverso/net/mpi_net.h
+++ next/include/multiverso/net/mpi_net.h
@@ -32,7 +32,7 @@ public:
     }
 
     void set_msg(MessagePtr& msg) { msg_ = std::move(msg); }
-
+    const MessagePtr& msg() const { return msg_; }
     void set_size(size_t size) { size_ = size; }
     size_t size() const { return size_; }
 
@@ -99,8 +99,8 @@ public:
       }
     }
     MPIMsgHandle* handle = new MPIMsgHandle();
-    size_t size = SendAsync(msg, handle);
     handle->set_msg(msg);
+    size_t size = SendAsync(handle->msg(), handle);
     handle->set_size(size);
     msg_handles_.push(handle);
     return size;
@@ -122,24 +122,6 @@ public:
   }
 
 private:
-  //size_t SendMsg(const MessagePtr& msg) {
-  //  size_t size = Message::kHeaderSize;
-  //  MPI_Send(msg->header(), Message::kHeaderSize, MPI_BYTE,
-  //    msg->dst(), 0, MPI_COMM_WORLD);
-  //  // Send multiple msg 
-  //  for (auto& blob : msg->data()) {
-  //    CHECK_NOTNULL(blob.data());
-  //    MPI_Send(blob.data(), static_cast<int>(blob.size()), MPI_BYTE, msg->dst(),
-  //      0, MPI_COMM_WORLD);
-  //    size += blob.size();
-  //  }
-  //  // Send an extra over tag indicating the finish of this Message
-  //  MPI_Send(&more_, sizeof(char), MPI_BYTE, msg->dst(),
-  //    0, MPI_COMM_WORLD);
-  //  // Log::Debug("MPI-Net: rank %d send msg size = %d\n", rank(), size+4);
-  //  return size + sizeof(char);
-  //}
-
   size_t SendAsync(const MessagePtr& msg, 
                    MPIMsgHandle* msg_handle) {
     size_t size = Message::kHeaderSize;
--- next/src/multiverso.cpp
+++ next/src/multiverso.cpp
@@ -20,4 +20,20 @@ int MultiversoRank() {
   return Zoo::Get()->rank();
 }
 
+void MV_Init(int* argc, char* argv[], int role) {
+  Zoo::Get()->Start(argc, argv, role);
+}
+
+void MV_ShutDown(bool finalize_net) {
+  Zoo::Get()->Stop(finalize_net);
+}
+
+void MV_Barrier() {
+  Zoo::Get()->Barrier();
+}
+
+int MV_Rank() {
+  return Zoo::Get()->rank();
+}
+
 }
