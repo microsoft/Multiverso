5fc09754db8748a72d38b578ae9094595c9fe0ec
Junjie Li <junli@microsoft.com>
2016-04-03 22:58:07 +0800

    add test for async buffer

--- next/Test/test_async_buffer.cpp
+++ next/Test/test_async_buffer.cpp
@@ -22,18 +22,37 @@ public:
 
         worker_array = new multiverso::SmoothArrayWorker<float>(array_size);
         server_array = new multiverso::SmoothArrayServer<float>(array_size);
-        p1 = new float[array_size]{0, 0, 0, 0, 0};
-        p2 = new float[array_size]{0, 1, 2, 3, 4};
+        p1 = new float[array_size]{};
+        p2 = new float[array_size]{};
+        
+        multiverso::MV_Barrier();
 
+        //    shared_array->Add(delta.data(), 10, 0.5f);
+        //    shared_array->Get(data, 10);
     }
 
     void TearDown() override{
         delete[]p2;
         delete[]p1;
-        // Need to delete array before shutdown?
+        // TODO: confirm: Shall we need to delete array before shutdown?
         //delete server_array;
         //delete worder_array;
+        multiverso::MV_Barrier();
         multiverso::MV_ShutDown();
+    }
+
+    bool elementwise_equal(float * array, size_t size, float target){
+        for (auto i = 0; i < size; ++i){
+            if (abs(array[i] - target) > 1e-6){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void elementwise_set(float * array, size_t size, float target){
+        for (auto i = 0; i < size; ++i) array[i] = target;
     }
 
 protected:
@@ -48,10 +67,25 @@ TEST_F(test_async_buffer, simple_test){
     auto size = array_size;
     multiverso::ASyncBuffer<float*> async_buffer(p1, p2,
         [worker, size](float * buf) -> void {
-        worker->Add(buf, size);
+        worker->Get(buf, size);
     });
     auto ready_buffer_1 = async_buffer.get_ready_buffer();
-    // once you call another get_ready_buffer, the previous ready buffer will be expired. So please keep the buffer content if you need before you calling another get_ready_buffer;
+    ASSERT_EQ(p1, ready_buffer_1);
+    ASSERT_TRUE(elementwise_equal(ready_buffer_1, size, 0));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));    // calculate updates
+    elementwise_set(ready_buffer_1, size, 1);
+    worker_array->Add(ready_buffer_1, size, 0);
+ 
     auto ready_buffer_2 = async_buffer.get_ready_buffer();
-    ASSERT_TRUE(true);
+    ASSERT_EQ(p2, ready_buffer_2);
+    ASSERT_TRUE(elementwise_equal(ready_buffer_2, size, 0));
+
+    ready_buffer_1 = async_buffer.get_ready_buffer();
+    ASSERT_EQ(p1, ready_buffer_1);
+    ASSERT_TRUE(elementwise_equal(ready_buffer_1, size, 1));
+
+    ready_buffer_2 = async_buffer.get_ready_buffer();
+    ASSERT_EQ(p2, ready_buffer_2);
+    ASSERT_TRUE(elementwise_equal(ready_buffer_1, size, 1));
+
 }
