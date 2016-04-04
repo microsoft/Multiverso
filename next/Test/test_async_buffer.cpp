3967ea1a05c4410e31823fafbbf9ffd63938e8f0
Junjie Li <junli@microsoft.com>
2016-04-04 15:40:01 +0800

    add tests and clean code

--- next/Test/test_async_buffer.cpp
+++ next/Test/test_async_buffer.cpp
@@ -1,16 +1,9 @@
 #include <gtest/gtest.h>
-#include <vector>
-
-#include <cctype>
-
-
 #include <multiverso/multiverso.h>
 #include <multiverso/table/smooth_array_table.h>
 #include <multiverso/util/async_buffer.h>
 
-
 const int array_size = 5;
-
 
 class test_async_buffer : public ::testing::Test    {
 public:
@@ -21,15 +14,15 @@ public:
         multiverso::MV_Init();
         worker_array = new multiverso::SmoothArrayWorker<float>(array_size);
         server_array = new multiverso::SmoothArrayServer<float>(array_size);
+        p0 = new float[array_size]{};
         p1 = new float[array_size]{};
-        p2 = new float[array_size]{};
         
         multiverso::MV_Barrier();
     }
 
     void TearDown() override{
-        delete[]p2;
         delete[]p1;
+        delete[]p0;
         // TODO: confirm: Shall we need to delete array before shutdown?
         //delete server_array;
         //delete worder_array;
@@ -37,7 +30,7 @@ public:
         multiverso::MV_ShutDown();
     }
 
-    bool elementwise_equal(float * array, size_t size, float target){
+    static bool elementwise_equal(float * array, size_t size, float target) {
         for (auto i = 0; i < size; ++i){
             if (abs(array[i] - target) > 1e-6){
                 return false;
@@ -47,44 +40,61 @@ public:
         return true;
     }
 
-    void elementwise_set(float * array, size_t size, float target){
+    static void elementwise_set(float * array, size_t size, float target) {
         for (auto i = 0; i < size; ++i) array[i] = target;
     }
 
 protected:
     multiverso::SmoothArrayWorker<float>* worker_array;
     multiverso::SmoothArrayServer<float>* server_array;
+    float* p0;
     float* p1;
-    float* p2;
 };
 
-TEST_F(test_async_buffer, simple_test){
+TEST_F(test_async_buffer, get_async_buffer_from_server){
     const auto& worker = this->worker_array;
     auto size = array_size;
-    multiverso::ASyncBuffer<float*> async_buffer(p1, p2,
+
+    // creates async_buffer
+    multiverso::ASyncBuffer<float*> async_buffer(p0, p1,
         [worker, size](float * buf) -> void {
         worker->Get(buf, size);
     });
 
+    // get ready buffer and submit an update A
     auto ready_buffer = async_buffer.get_ready_buffer();
-    ASSERT_EQ(p1, ready_buffer);
+    ASSERT_EQ(p0, ready_buffer);
     ASSERT_TRUE(elementwise_equal(ready_buffer, size, 0));
     std::this_thread::sleep_for(std::chrono::milliseconds(100));    // calculate updates
     elementwise_set(ready_buffer, size, 1);
     worker_array->Add(ready_buffer, size, 0);
  
+    // get ready buffer and submit an update B, the buffer content should NOT include changes from update A
     ready_buffer = async_buffer.get_ready_buffer();
-    ASSERT_EQ(p2, ready_buffer);
+    ASSERT_EQ(p1, ready_buffer);
     ASSERT_TRUE(elementwise_equal(ready_buffer, size, 0));
     std::this_thread::sleep_for(std::chrono::milliseconds(100));    // calculate updates
     elementwise_set(ready_buffer, size, 1);
     worker_array->Add(ready_buffer, size, 0);
 
+    // get ready buffer, the buffer content should include changes from update A
     ready_buffer = async_buffer.get_ready_buffer();
-    ASSERT_EQ(p1, ready_buffer);
+    ASSERT_EQ(p0, ready_buffer);
     ASSERT_TRUE(elementwise_equal(ready_buffer, size, 1));
 
+    // get ready buffer, the buffer content should include changes from update A and update B
     ready_buffer = async_buffer.get_ready_buffer();
-    ASSERT_EQ(p2, ready_buffer);
+    ASSERT_EQ(p1, ready_buffer);
     ASSERT_TRUE(elementwise_equal(ready_buffer, size, 2));
+
+    // stop prefetch and release related resource
+    async_buffer.stop_prefetch();
+
+    // restart prefetch after stopping
+    ready_buffer = async_buffer.get_ready_buffer();
+    ASSERT_EQ(p0, ready_buffer);
+    ASSERT_TRUE(elementwise_equal(ready_buffer, size, 2));
+
+    // stop prefetch and release related resource
+    async_buffer.stop_prefetch();
 }
--- next/include/multiverso/util/async_buffer.h
+++ next/include/multiverso/util/async_buffer.h
@@ -1,85 +1,106 @@
-#ifndef MULTIVERSO_DOUBLE_BUFFER_H_
-#define MULTIVERSO_DOUBLE_BUFFER_H_
-
-#include <thread>
+#ifndef  INCLUDE_MULTIVERSO_UTIL_ASYNC_BUFFER_H_
+#define  INCLUDE_MULTIVERSO_UTIL_ASYNC_BUFFER_H_
 
 #include <multiverso\table_interface.h>
 #include <multiverso\util\waiter.h>
+#include <thread>
 
 namespace multiverso {
 
-    template<typename BufferType = void*>
-	class ASyncBuffer{
+template<typename BufferType = void*>
+class ASyncBuffer{
+ public:
+    // Creates an async buffer
+    // buffer1 and buffer2: buffers used to save data from server
+    // fill_buffer_action: action to fill a given buffer.
+    ASyncBuffer(BufferType& buffer0, BufferType& buffer1,
+        std::function<void(BufferType&)> fill_buffer_action)
+        : buffer0_{ buffer0 }, buffer1_{ buffer1 },
+        fill_buffer_func_{ fill_buffer_action } {
+        init();
+    }
 
-    public:
-        ASyncBuffer(BufferType& buffer0, BufferType& buffer1, std::function<void(BufferType&)> fill_buffer_func)
-            : buffer0_{ buffer0 }, buffer1_{ buffer1 }, fill_buffer_func_{ fill_buffer_func }, ready_waiter_{ 0 }, new_task_waiter_{ 1 }, current_task_(FILL_BUFFER1){
-            thread_ = new std::thread(&ASyncBuffer<BufferType>::fill_buffer_routine, this);
-            prefetch_next();
+    // Returns the ready buffer.
+    // This function also automatically starts to prefetch data 
+    // for the other buffer.
+    BufferType& get_ready_buffer() {
+        if (thread_ == nullptr) {
+            init();
         }
 
-        BufferType& get_ready_buffer(){
-            ready_waiter_.Wait();
-            auto ready_buffer = get_buffer_to_fill(current_task_);
+        ready_waiter_.Wait();
+        auto ready_buffer = get_buffer_to_fill(current_task_);
+        prefetch_next_async();
+        return ready_buffer;
+    }
 
-            prefetch_next();
-            return ready_buffer;
-        }
-
-        ~ASyncBuffer(){
+    ~ASyncBuffer() {
+        if (thread_ != nullptr) {
             stop_prefetch();
         }
+    }
 
-    protected:
-        enum TaskType{
-            FILL_BUFFER0,
-            FILL_BUFFER1,
-            STOP_THREAD
-        };
+    // Stops prefetch and releases related resource
+    void stop_prefetch() {
+        ready_waiter_.Wait();
+        current_task_ = STOP_THREAD;
+        new_task_waiter_.Notify();
+        thread_->join();
+        thread_ = nullptr;
+    }
 
-    protected:
-        void prefetch_next(){
-            current_task_ = (current_task_ == FILL_BUFFER1) ? FILL_BUFFER0 : FILL_BUFFER1;
-            ready_waiter_.Reset(1);
-            new_task_waiter_.Notify();
-        }
+ protected:
+    enum TaskType {
+        FILL_BUFFER0,
+        FILL_BUFFER1,
+        STOP_THREAD
+    };
 
-        void stop_prefetch(){
-            ready_waiter_.Wait();
-            current_task_ = STOP_THREAD;
-            new_task_waiter_.Notify();
-            thread_->join();
-        }
+ protected:
+    void init() {
+        ready_waiter_.Reset(0);
+        new_task_waiter_.Reset(1);
+        current_task_ = FILL_BUFFER1;
+        thread_ = new std::thread(&ASyncBuffer<BufferType>::fill_buffer_routine, this);
+        prefetch_next_async();
+    }
 
-        BufferType& get_buffer_to_fill(TaskType task){
-            CHECK(task != STOP_THREAD);
-            return task == FILL_BUFFER0 ? buffer0_ : buffer1_;
-        }
+    void prefetch_next_async() {
+        current_task_ = (current_task_ == FILL_BUFFER1) ? FILL_BUFFER0 : FILL_BUFFER1;
+        ready_waiter_.Reset(1);
+        new_task_waiter_.Notify();
+    }
 
-    private:
-        BufferType& buffer0_;
-        BufferType& buffer1_;
-        std::function<void(BufferType&)> fill_buffer_func_;
-        Waiter ready_waiter_;
-        Waiter new_task_waiter_;
-        TaskType current_task_;
-        std::thread * thread_;
+    BufferType& get_buffer_to_fill(TaskType task) {
+        CHECK(task != STOP_THREAD);
+        return task == FILL_BUFFER0 ? buffer0_ : buffer1_;
+    }
 
-    private:
-        void fill_buffer_routine() {
-            while (true) {
-                new_task_waiter_.Wait();
-                if (current_task_ == STOP_THREAD){
-                    break;
-                }
+ private:
+    BufferType& buffer0_;
+    BufferType& buffer1_;
+    std::function<void(BufferType&)> fill_buffer_func_;
+    Waiter ready_waiter_;
+    Waiter new_task_waiter_;
+    TaskType current_task_;
+    std::thread * thread_;
 
-                fill_buffer_func_(get_buffer_to_fill(current_task_));
-                ready_waiter_.Notify();
-                new_task_waiter_.Reset(1);
+ private:
+    void fill_buffer_routine() {
+        while (true) {
+            new_task_waiter_.Wait();
+            if (current_task_ == STOP_THREAD) {
+                break;
             }
+
+            fill_buffer_func_(get_buffer_to_fill(current_task_));
+            ready_waiter_.Notify();
+            new_task_waiter_.Reset(1);
         }
-	};
-}
+    }
+};
+
+} // namespace multiverso
 
 
-#endif //MULTIVERSO_DOUBLE_BUFFER_H_
+#endif  // INCLUDE_MULTIVERSO_UTIL_ASYNC_BUFFER_H_
