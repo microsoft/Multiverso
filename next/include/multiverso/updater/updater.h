25ad346ae08e9c7264bf8e368832f16b8ff38c06
feiga <gf0109@gmail.com>
2016-04-07 14:41:47 +0800

    add file

--- next/include/multiverso/updater/updater.h
+++ next/include/multiverso/updater/updater.h
@@ -14,10 +14,10 @@ public:
     CopyFrom(data, size);
   }
 
-  float momentum() const { return data_[0].f; }
-  void set_momentum(float momentum) { data_[0].f = momentum; }
   float learning_rate() const { return data_[1].f; }
   void set_learning_rate(float lr) { data_[1].f = lr; }
+  float momentum() const { return data_[0].f; }
+  void set_momentum(float momentum) { data_[0].f = momentum; }
 
   const char* data() const { return reinterpret_cast<const char*>(&data_[0]); }
   size_t size() const { return kSize * sizeof(InternalType); }
@@ -27,11 +27,17 @@ public:
   }
 private:
   static const size_t kSize = 2;
-  // to make it easy serialize and deserialize
+  // Option can be either int type or float, 
+  // to make it easy to serialize and deserialize
   union InternalType{
     int i;
     float f;
   };
+
+  // May add other option for future potential update algorithm
+  // 0: learning rate
+  // 1: momentum
+  // ...
   InternalType data_[kSize];
 };
 
