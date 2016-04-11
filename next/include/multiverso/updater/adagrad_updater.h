aa3572d2581b2aaa576d16d2d366e5906b921a82
Qiwei Ye <qiwye@microsoft.com>
2016-04-11 13:50:55 +0800

    adgrad updater

--- next/include/multiverso/updater/adagrad_updater.h
+++ next/include/multiverso/updater/adagrad_updater.h
@@ -10,13 +10,29 @@ namespace multiverso {
 template <typename T>
 class AdaGradUpdater : public Updater<T> {
 public:
-  explicit AdaGradUpdater(size_t size) {}
+  explicit AdaGradUpdater(size_t size):
+    e(1e-6) {  
+    historic_g_sqr_.resize(MV_NumWorkers());
+    for (auto s : historic_g_sqr_){
+      s.resize(size);
+    }
+  }
   void Update(size_t num_element, T* data, T* delta, 
               UpdateOption* option, size_t offset) override {
+    for (size_t index = 0; index < num_element; ++index) {
 
+      historic_g_sqr_[option->worker_id()][index + offset] -=
+        delta[index] * delta[index] / option->learning_rate() / 
+        option->learning_rate();
+
+      data[index + offset] -= option->rho() /
+        std::sqrtf(historic_g_sqr_[option->worker_id()][index + offset] + e) *
+        delta[index] / option->learning_rate();
+    }
   }
 protected:
     std::vector< std::vector<T>> historic_g_sqr_;
+    float e;
 };
 
 }
