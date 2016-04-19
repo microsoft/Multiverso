05c01fb74eed999aeea117523faec85e47f676e6
feiga <gf0109@gmail.com>
2016-04-19 21:59:48 +0800

    remove sync server since it's a special case of backup sync server when backup worker num = 0

--- src/server.cpp
+++ src/server.cpp
@@ -61,105 +61,109 @@ void Server::ProcessAdd(MessagePtr& msg) {
 // If worker k has add delta to server j times when its i-th Get 
 // then the server will return the parameter after all K 
 // workers finished their j-th update
-class SyncServer : public Server {
-public:
-  SyncServer() : Server() {
-    int num_worker = Zoo::Get()->num_workers();
-    worker_get_clocks_.reset(new VectorClock(num_worker));
-    worker_add_clocks_.reset(new VectorClock(num_worker));
-  }
 
-  // make some modification to suit to the sync server
-  // please not use in other place, may different with the general vector clock
-  class VectorClock {
-  public:
-    explicit VectorClock(int n) : 
-      local_clock_(n, 0), global_clock_(0), size_(0) {}
+// TODO(feiga): to delete this, SyncServer is a special case for 
+//              BackupWorkerSyncServer
 
-    // Return true when all clock reach a same number
-    virtual bool Update(int i) {
-      ++local_clock_[i];
-      if (global_clock_ < *(std::min_element(std::begin(local_clock_),
-        std::end(local_clock_)))) {
-        ++global_clock_;
-        if (global_clock_ == *(std::max_element(std::begin(local_clock_),
-          std::end(local_clock_)))) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    std::string DebugString() {
-      std::string os = "global ";
-      os += std::to_string(global_clock_) + " local: ";
-      for (auto i : local_clock_) os += std::to_string(i) + " ";
-      return os;
-    }
-
-    int local_clock(int i) const { return local_clock_[i]; }
-    int global_clock() const { return global_clock_; }
-
-  protected:
-    std::vector<int> local_clock_;
-    int global_clock_;
-    int size_;
-  };
-protected:
-  void ProcessAdd(MessagePtr& msg) override {
-    // 1. Before add: cache faster worker
-    int worker = Zoo::Get()->rank_to_worker_id(msg->src());
-    if (worker_get_clocks_->local_clock(worker) >
-        worker_get_clocks_->global_clock()) {
-      msg_add_cache_.Push(msg);
-      return;
-    }
-    // 2. Process Add
-    Server::ProcessAdd(msg);
-    // 3. After add: process cached process get if necessary
-    if (worker_add_clocks_->Update(worker)) {
-      CHECK(msg_add_cache_.Empty());
-      while (!msg_get_cache_.Empty()) {
-        MessagePtr get_msg;
-        CHECK(msg_get_cache_.TryPop(get_msg));
-        int get_worker = Zoo::Get()->rank_to_worker_id(get_msg->src());
-        Server::ProcessGet(get_msg);
-        worker_get_clocks_->Update(get_worker);
-      }
-    }
-  }
-
-  void ProcessGet(MessagePtr& msg) override {
-    // 1. Before get: cache faster worker
-    int worker = Zoo::Get()->rank_to_worker_id(msg->src());
-    if (worker_add_clocks_->local_clock(worker) >
-        worker_add_clocks_->global_clock()) {
-      // Will wait for other worker finished Add
-      msg_get_cache_.Push(msg);
-      return;
-    }
-    // 2. Process Get
-    Server::ProcessGet(msg);
-    // 3. After get: process cached process add if necessary
-    if (worker_get_clocks_->Update(worker)) {
-      CHECK(msg_get_cache_.Empty());
-      while (!msg_add_cache_.Empty()) {
-        MessagePtr add_msg;
-        CHECK(msg_add_cache_.TryPop(add_msg));
-        int add_worker = Zoo::Get()->rank_to_worker_id(add_msg->src());
-        Server::ProcessAdd(add_msg);
-        worker_add_clocks_->Update(add_worker);
-      }
-    }
-  }
-
-private:
-  std::unique_ptr<VectorClock> worker_get_clocks_;
-  std::unique_ptr<VectorClock> worker_add_clocks_;
-
-  MtQueue<MessagePtr> msg_add_cache_;
-  MtQueue<MessagePtr> msg_get_cache_;
-};
+//class SyncServer : public Server {
+//public:
+//  SyncServer() : Server() {
+//    int num_worker = Zoo::Get()->num_workers();
+//    worker_get_clocks_.reset(new VectorClock(num_worker));
+//    worker_add_clocks_.reset(new VectorClock(num_worker));
+//  }
+//
+//  // make some modification to suit to the sync server
+//  // please not use in other place, may different with the general vector clock
+//  class VectorClock {
+//  public:
+//    explicit VectorClock(int n) : 
+//      local_clock_(n, 0), global_clock_(0), size_(0) {}
+//
+//    // Return true when all clock reach a same number
+//    virtual bool Update(int i) {
+//      ++local_clock_[i];
+//      if (global_clock_ < *(std::min_element(std::begin(local_clock_),
+//        std::end(local_clock_)))) {
+//        ++global_clock_;
+//        if (global_clock_ == *(std::max_element(std::begin(local_clock_),
+//          std::end(local_clock_)))) {
+//          return true;
+//        }
+//      }
+//      return false;
+//    }
+//
+//    std::string DebugString() {
+//      std::string os = "global ";
+//      os += std::to_string(global_clock_) + " local: ";
+//      for (auto i : local_clock_) os += std::to_string(i) + " ";
+//      return os;
+//    }
+//
+//    int local_clock(int i) const { return local_clock_[i]; }
+//    int global_clock() const { return global_clock_; }
+//
+//  protected:
+//    std::vector<int> local_clock_;
+//    int global_clock_;
+//    int size_;
+//  };
+//protected:
+//  void ProcessAdd(MessagePtr& msg) override {
+//    // 1. Before add: cache faster worker
+//    int worker = Zoo::Get()->rank_to_worker_id(msg->src());
+//    if (worker_get_clocks_->local_clock(worker) >
+//        worker_get_clocks_->global_clock()) {
+//      msg_add_cache_.Push(msg);
+//      return;
+//    }
+//    // 2. Process Add
+//    Server::ProcessAdd(msg);
+//    // 3. After add: process cached process get if necessary
+//    if (worker_add_clocks_->Update(worker)) {
+//      CHECK(msg_add_cache_.Empty());
+//      while (!msg_get_cache_.Empty()) {
+//        MessagePtr get_msg;
+//        CHECK(msg_get_cache_.TryPop(get_msg));
+//        int get_worker = Zoo::Get()->rank_to_worker_id(get_msg->src());
+//        Server::ProcessGet(get_msg);
+//        worker_get_clocks_->Update(get_worker);
+//      }
+//    }
+//  }
+//
+//  void ProcessGet(MessagePtr& msg) override {
+//    // 1. Before get: cache faster worker
+//    int worker = Zoo::Get()->rank_to_worker_id(msg->src());
+//    if (worker_add_clocks_->local_clock(worker) >
+//        worker_add_clocks_->global_clock()) {
+//      // Will wait for other worker finished Add
+//      msg_get_cache_.Push(msg);
+//      return;
+//    }
+//    // 2. Process Get
+//    Server::ProcessGet(msg);
+//    // 3. After get: process cached process add if necessary
+//    if (worker_get_clocks_->Update(worker)) {
+//      CHECK(msg_get_cache_.Empty());
+//      while (!msg_add_cache_.Empty()) {
+//        MessagePtr add_msg;
+//        CHECK(msg_add_cache_.TryPop(add_msg));
+//        int add_worker = Zoo::Get()->rank_to_worker_id(add_msg->src());
+//        Server::ProcessAdd(add_msg);
+//        worker_add_clocks_->Update(add_worker);
+//      }
+//    }
+//  }
+//
+//private:
+//  std::unique_ptr<VectorClock> worker_get_clocks_;
+//  std::unique_ptr<VectorClock> worker_add_clocks_;
+//
+//  MtQueue<MessagePtr> msg_add_cache_;
+//  MtQueue<MessagePtr> msg_get_cache_;
+//};
 
 class WithBackupSyncServer : public Server {
 public:
@@ -174,19 +178,22 @@ public:
     }
     Log::Info("Sync with backup worker start: num_sync_worker = %d," 
               "num_total_worker = %d\n", num_sync_worker_, num_worker_);
-    worker_get_clocks_.reset(new SyncServer::VectorClock(num_worker_));
-    worker_add_clocks_.reset(new VectorClock(num_worker_, num_sync_worker_));
+    worker_get_clocks_.reset(new VectorClock(num_worker_));
+    worker_add_clocks_.reset(new VectorClock(
+      num_worker_, num_worker_ - num_sync_worker_));
   }
 
   // make some modification to suit to the sync server
   // please not use in other place, may different with the general vector clock
   class VectorClock {
   public:
-    VectorClock(int num_worker, int num_sync_worker) :
+    VectorClock(int num_worker, int num_backup_worker = 0) :
       local_clock_(num_worker, 0), global_clock_(0), num_worker_(num_worker), 
-      num_sync_worker_(num_sync_worker), progress_(0) {}
+      num_sync_worker_(num_worker - num_backup_worker), progress_(0) {}
 
-    // Return true when all clock reach a same number
+    // Return true when global clock meet the sync condition
+    // sync: all worker reach the same clock
+    // backup-worker-sync: sync-workers reach the same clock
     virtual bool Update(int i) {
       if (local_clock_[i]++ == global_clock_) {
         ++progress_;
@@ -277,7 +284,7 @@ protected:
   }
 
 private:
-  std::unique_ptr<SyncServer::VectorClock> worker_get_clocks_;
+  std::unique_ptr<VectorClock> worker_get_clocks_;
   std::unique_ptr<VectorClock> worker_add_clocks_;
 
   MtQueue<MessagePtr> msg_add_cache_;
@@ -293,12 +300,10 @@ Server* Server::GetServer() {
     Log::Info("Create a async server\n");
     return new Server();
   }
-  if (MV_CONFIG_backup_worker_ratio > 0.0) {
-    Log::Info("Create a sync server with backup worker\n");
-    return new WithBackupSyncServer();
-  }
+  // if (MV_CONFIG_backup_worker_ratio > 0.0) {
   Log::Info("Create a sync server\n");
-  return new SyncServer();
+  return new WithBackupSyncServer();
+  // }
 }
 
 }  // namespace multiverso
