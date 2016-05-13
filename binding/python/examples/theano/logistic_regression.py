420dc3e4acfbae833c6639d4b20d13bacbe358fd
Young <afe.young@gmail.com>
2016-05-13 14:43:03 +0900

    add licence

--- binding/python/examples/theano/logistic_regression.py
+++ binding/python/examples/theano/logistic_regression.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python
 # coding:utf8
+"""
+
+This code is adapted from Deep Learning Tutorials
+http://deeplearning.net/tutorial/logreg.html
+
+Copyright (c) 2008–2013, Theano Development Team All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    Neither the name of Theano nor the names of its contributors may be used to
+    endorse or promote products derived from this software without specific
+    prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ‘’AS IS’’ AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+"""
 
 """
 This tutorial introduces logistic regression using Theano and stochastic
@@ -54,7 +86,7 @@ import theano
 import theano.tensor as T
 
 from multiverso.api import MatrixTableHandler, ArrayTableHandler, MV_Init, MV_Barrier, MV_ShutDown, \
-                            MV_NumWorkers, MV_WorkerId
+    MV_NumWorkers, MV_WorkerId
 
 
 class LogisticRegression(object):
@@ -361,7 +393,6 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
 
     W_tbh = MatrixTableHandler(classifier.n_in, classifier.n_out)
     b_tbh = ArrayTableHandler(classifier.n_out)
-
 
     while (epoch < n_epochs) and (not done_looping):
         epoch = epoch + 1
