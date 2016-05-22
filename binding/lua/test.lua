4cf85f6fe4b24a2e6bae726059da3e607966a492
huxuan <i@huxuan.org>
2016-05-23 03:46:12 +0900

    Matrix Table API and unittest.

--- binding/lua/test.lua
+++ binding/lua/test.lua
@@ -7,9 +7,14 @@ mv = require('multiverso')
 local mv_test = torch.TestSuite()
 local mv_tester = torch.Tester()
 
+function Set(list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+
 function mv_test.testArray()
     size = 100000
-    mv.init()
     tbh = mv.ArrayTableHandler:new(size)
     mv.barrier()
 
@@ -19,9 +24,48 @@ function mv_test.testArray()
         tbh:add(torch.range(1, size))
         mv.barrier()
     end
-
-    mv.shutdown()
 end
 
+function mv_test.testMatrix()
+    num_row = 11
+    num_col = 10
+    size = num_row * num_col
+    num_workers = mv.num_workers()
+    tbh = mv.MatrixTableHandler:new(num_row, num_col)
+    mv.barrier()
+
+    for i = 1, 20 do
+        row_ids = {0, 1, 5, 10}
+        row_ids_set = Set(row_ids)
+        tbh:add(torch.range(1, size))
+        data = torch.range(
+            row_ids[1] * num_col + 1,
+            row_ids[1] * num_col + num_col
+        )
+        for j = 2, #row_ids do
+            row_id = row_ids[j]
+            data = torch.cat(data, torch.range(
+                row_id * num_col + 1,
+                row_id * num_col + num_col
+            ))
+        end
+        tbh:add(data, row_ids)
+        mv.barrier()
+        data = tbh:get()
+        mv.barrier()
+        for j = 1, data:size()[1] do
+            for k = 1, data:size()[2] do
+                expected = ((j - 1) * num_col + k) * i * num_workers
+                if row_ids_set[j - 1] then
+                    expected = expected + ((j - 1) * num_col + k) * i * num_workers
+                end
+                mv_tester:eq(expected, data[j][k])
+            end
+        end
+    end
+end
+
+mv.init()
 mv_tester:add(mv_test)
 mv_tester:run()
+mv.shutdown()
