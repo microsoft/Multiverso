9b13f8780348b8a781bf5f78c64da8065994877d
Young <afe.young@gmail.com>
2016-09-26 01:08:25 +0800

    add freq parameter to callbacks

--- binding/python/multiverso/theano_ext/keras_ext/callbacks.py
+++ binding/python/multiverso/theano_ext/keras_ext/callbacks.py
@@ -15,13 +15,25 @@ class MVCallback(Callback):
               nb_epoch=nb_epoch,
               validation_data=(X_test, Y_test),
               shuffle=True,
-              callbacks=[mvcallback(model)])
+              callbacks=[mvcallback(model, freq=1)])
     ```
     '''
-    def __init__(self, model):
+    def __init__(self, model, freq=1):
+        '''Initialize the MVCallback class
+
+        The `model` should be the be a keras model
+        The `freq` should be the update frequency of the parameters. For
+        example, `freq=3` means update the parameters every 3 mini-batch.
+        '''
         super(MVCallback, self).__init__()
         self.kpm = KerasParamManager(model)
+        self.cur_n = 0
+        if freq < 0:
+            raise ValueError("Frequency must be an integer greater than 0.")
+        self.freq = freq
 
     def on_batch_end(self, batch, logs={}):
         '''sync all parameters at the end of every batch'''
-        self.kpm.sync_all_param()
+        self.cur_n = (self.cur_n + 1) % self.freq
+        if self.cur_n % self.freq == 0:
+            self.kpm.sync_all_param()
