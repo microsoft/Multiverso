ca23ef56d1038bc8d15cc74b3fd271d8c3650764
Qiwei Ye <qiwye@microsoft.com>
2016-10-27 19:23:28 +0800

    adding DC-ASGD support

--- src/updater/updater.cpp
+++ src/updater/updater.cpp
@@ -1,15 +1,24 @@
 #include "multiverso/updater/updater.h"
+// TODO(qiwye) to make this a option in CMakelist
+//#define ENABLE_DCASGD
 
 #include "multiverso/updater/adagrad_updater.h"
 #include "multiverso/updater/momentum_updater.h"
+#ifdef ENABLE_DCASGD
+#include "multiverso/updater/dcasgd/dcasgd_updater.h"
+#endif
 #include "multiverso/updater/sgd_updater.h"
 #include "multiverso/util/configure.h"
 #include "multiverso/util/log.h"
+
 
 namespace multiverso {
 
 MV_DEFINE_string(updater_type, "default", "multiverso server updater type");
 MV_DEFINE_int(omp_threads, 4 , "#theads used by openMP for updater");
+#ifdef ENABLE_DCASGD
+MV_DEFINE_bool(is_pipelined, false, "Only used for CNTK - DCASGD");
+#endif
 
 template <typename T>
 void Updater<T>::Update(size_t num_element, T* data, T* delta,
@@ -41,6 +50,9 @@ Updater<T>* Updater<T>::GetUpdater(size_t size) {
   if (type == "sgd") return new SGDUpdater<T>(size);
   if (type == "adagrad") return new AdaGradUpdater<T>(size);
   if (type == "momentum_sgd") return new MomentumUpdater<T>(size);
+#ifdef ENABLE_DCASGD
+  if (type == "dcasgd") return new DCASGDUpdater<T>(size, MV_CONFIG_is_pipelined);
+#endif
   // Default: simple updater
   return new Updater<T>();
 }
