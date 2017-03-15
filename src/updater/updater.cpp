b9973ef3a8698b51f4f941d7a76f78ebe6b6c09b
Shuxin Zheng (MSR Student-Person Consulting) <v-shuzhe@microsoft.com>
2017-03-15 19:48:14 +0800

    fix asgd bugs

--- src/updater/updater.cpp
+++ src/updater/updater.cpp
@@ -14,9 +14,7 @@ namespace multiverso {
 
 MV_DEFINE_string(updater_type, "default", "multiverso server updater type");
 MV_DEFINE_int(omp_threads, 4 , "#theads used by openMP for updater");
-#ifdef ENABLE_DCASGD
 MV_DEFINE_bool(is_pipelined, false, "Only used for CNTK - DCASGD");
-#endif
 
 template <typename T>
 void Updater<T>::Update(size_t num_element, T* data, T* delta,
@@ -48,9 +46,7 @@ Updater<T>* Updater<T>::GetUpdater(size_t size) {
   if (type == "sgd") return new SGDUpdater<T>(size);
   if (type == "adagrad") return new AdaGradUpdater<T>(size);
   if (type == "momentum_sgd") return new MomentumUpdater<T>(size);
-#ifdef ENABLE_DCASGD
   if (type == "dcasgd") return new DCASGDUpdater<T>(size, MV_CONFIG_is_pipelined);
-#endif
   // Default: simple updater
   return new Updater<T>();
 }
